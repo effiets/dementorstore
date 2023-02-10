545b4ee07a7770ad68a064ae2fbd93f5164bff48
effiets <efoula86@gmail.com>
2023-02-10 13:32:19 +0200

    Setting up my repository

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+
+const app = express();
+
+app.get(`/`, (req, res) =>{
+    res.status(200).send('this is the first response dudes!!!!')
+})
+
+
+const port = 3000;
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}.....`)
+})
